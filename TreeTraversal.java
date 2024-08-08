ef8ace21530837a193ab6ff158615c66083c476d
chandrasudiksha <116143966+chandrasudiksha@users.noreply.github.com>
2024-08-07 21:21:02 -0700

    Update TreeTraversal.java

--- TreeTraversal.java
+++ TreeTraversal.java
@@ -41,6 +41,7 @@ public class TreeTraversal{
         root.right.right.left=new TreeNode(9);
         root.right.right.right=new TreeNode(10);
         TreeTraversal tree=new TreeTraversal();
+        System.out.println("Question 1");
         System.out.print("Preorder traversal: ");
         tree.preorder(root);
         System.out.println();
@@ -49,6 +50,24 @@ public class TreeTraversal{
         System.out.println();
         System.out.print("Postorder traversal: ");
         tree.postorder(root);
+        System.out.println("\nQuestion 2");
+        TreeNode root2=new TreeNode(4);
+        root2.left=new TreeNode(2);
+        root2.right=new TreeNode(5);
+        root2.left.left=new TreeNode(3);
+        root2.left.left.right=new TreeNode(9);
+        root2.left.left.right.left=new TreeNode(1);
+        root2.right.left=new TreeNode(7);
+        root2.right.right=new TreeNode(6);
+        root2.right.right.left=new TreeNode(8);
+        System.out.print("Preorder traversal: ");
+        tree.preorder(root2);
+        System.out.println();
+        System.out.print("Inorder traversal: ");
+        tree.inorder(root2);
+        System.out.println();
+        System.out.print("Postorder traversal: ");
+        tree.postorder(root2);
         System.out.println();
     }
 }
